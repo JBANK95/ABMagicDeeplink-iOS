80dc0af3525c9208c153b6698a74b6f8e0ea9a56
kiransar <ksarv05@safeway.com>
2022-09-02 21:08:38 +0530

    Initial project setup

---
+++ ABMagicDeeplink/ABMagicDeeplink.h
@@ -0,0 +1,18 @@
+//
+//  ABMagicDeeplink.h
+//  ABMagicDeeplink
+//
+//  Created by Kiran Sarvaiya on 02/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ABMagicDeeplink.
+FOUNDATION_EXPORT double ABMagicDeeplinkVersionNumber;
+
+//! Project version string for ABMagicDeeplink.
+FOUNDATION_EXPORT const unsigned char ABMagicDeeplinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ABMagicDeeplink/PublicHeader.h>
+
+
