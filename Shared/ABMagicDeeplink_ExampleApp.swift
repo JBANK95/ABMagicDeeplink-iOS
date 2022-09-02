dcff5d5b573b01837038c2cd37f2216be02cd058
kiransar <ksarv05@safeway.com>
2022-09-02 22:09:33 +0530

    Added Example Code

---
+++ Shared/ABMagicDeeplink_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABMagicDeeplink_ExampleApp.swift
+//  Shared
+//
+//  Created by Kiran Sarvaiya on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ABMagicDeeplink_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
